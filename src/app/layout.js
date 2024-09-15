0460512d772487a24def8ad9cd9387a26fa7899a
Md Ismail Hossen <smile@smile.smile>
2024-09-15 17:54:26 +0600

    setup with bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,16 @@
-import CommonLayout from "@/components/client-view/common-layout";
+import localFont from "next/font/local";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -12,8 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <CommonLayout>{children}</CommonLayout>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
